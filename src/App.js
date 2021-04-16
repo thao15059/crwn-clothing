e22017229a0121d4a376a8390dfca53704083d35
Thao15059 <thao15059@gmail.com>
2021-04-16 15:22:46 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Route } from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => {
+  return (
+  <div><h1>Hats page</h1></div>
+  )
+}
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,9 +50,9 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, id, imageUrl, size}) => {
+                    this.state.sections.map(({id, ...otherSectionProps}) => {
                       return (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                        <MenuItem key={id} {...otherSectionProps} />
                       )
                     })
                 }
