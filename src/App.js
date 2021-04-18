7306e6f65080e5cfdc1313f381d4393e61be608b
Thao15059 <thao15059@gmail.com>
2021-04-18 12:47:18 +0700

    add header and menu bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+
+import Header from './components/header/header.component'
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -8,8 +10,11 @@ import ShopPage from './pages/shop/shop.component.jsx';
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop/" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
