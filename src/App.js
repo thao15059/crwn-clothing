5a63cc4a26e3ec7dc891b748bfab0e11a94ed135
thao15059 <thao15059@gmail.com>
2021-04-20 22:48:32 +0700

    add sign up function on firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAngSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 
 
 class App extends React.Component {
@@ -23,8 +23,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        // just user ref not include data
+        const userRef = await createUserProfileDocument(userAuth); 
+        // get data from userRef
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: userRef.uid,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -14,6 +14,29 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+    if(!userAuth) return;
+    // get ref obj from firebase
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    // check ref obj exist in firebase
+    const userSnapShot = await userRef.get();
+
+    // not exist
+    if (!userSnapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            // create new user
+            await userRef.set({ displayName, email, createdAt, ...additionalData })
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return userRef;
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
